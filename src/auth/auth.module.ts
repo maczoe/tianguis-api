75d7366b80915782a70a899f560a136658fafc66
Marco Linares <macolinares@gmail.com>
2022-09-24 15:56:04 -0600

    Cambios auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth/auth.controller';
 import { AuthService } from './auth/auth.service';
+import { GoogleStrategy } from './google/google.strategy';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, GoogleStrategy],
 })
 export class AuthModule {}
